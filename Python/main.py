2bea4f2212741b8c033c724b796184757e174a9f
Remi <rku16@ic.ac.uk>
2019-02-28 17:24:27 +0000

    updated main python

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,24 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import KFold
 
+def read_input_pizza(filename):
+    """Reads the input of a Pizza problem.
 
+    returns:
+
+    R: number of Rows of pizza grid
+    C: number of Cols of pizza grid
+    L: Lowest number of each ingredients per slice
+    H: Highest number of cells per slice
+    pizza: the pizza grid (1 == tomato, 0 == mushroom)
+    """
+    lines = open(filename).readlines()
+    R, C, L, H = [int(val) for val in lines[0].split()]
+    pizza = np.array([list(map(lambda item: 1 if item == 'T' else 0, row.strip())) for row in lines[1:]])
+    return R, C, L, H, pizza
 
 if __name__ == "__main__":
     main()
