f22c62e65844d7ac325b5fd06337300ab5d70264
Guy Leroy <guy.leroy99@gmail.com>
2019-02-28 21:20:30 +0000

    improvements java

--- Java/src/Slideshow.java
+++ Java/src/Slideshow.java
@@ -1,10 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 public class Slideshow {
+
     private List<Slide> slides;
+    private int score;
 
     public Slideshow(List<Slide> slides) {
         this.slides = slides;
+        this.score = -1;
     }
 
     public int calculateScoreSlideshow(){
@@ -17,19 +25,72 @@ public class Slideshow {
             result += this.slides.get(i).calcuteScoreTransition(prev);
             prev = this.slides.get(i);
         }
+        this.score = result;
         return result;
     }
 
+    public int swap2slidesRand(){
+        int scoreOriginal = this.score;
+        Random r = new Random();
+        int index1 = r.nextInt(this.slides.size());
+        int index2 = r.nextInt(this.slides.size());
+        int firstpass = 0;
 
-    public void outputSolution(){
-        System.out.println(this.slides.size());
-        for(Slide s : slides){
+        if(index1 != 0) {
+            firstpass -= this.slides.get(index1).calcuteScoreTransition(this.slides.get(index1 - 1));
+        }
+        if(index2 != 0) {
+            firstpass -= this.slides.get(index2).calcuteScoreTransition(this.slides.get(index2 - 1));
+        }
+        if(index1 != this.slides.size()-1) {
+            firstpass -= this.slides.get(index1).calcuteScoreTransition(this.slides.get(index1 + 1));
+        }
+        if(index2 != this.slides.size()-1) {
+            firstpass -= this.slides.get(index2).calcuteScoreTransition(this.slides.get(index2 + 1));
+        }
+
+        if(index2 != 0) {
+            firstpass += this.slides.get(index1).calcuteScoreTransition(this.slides.get(index2 - 1));
+        }
+        if(index1 != 0) {
+            firstpass += this.slides.get(index2).calcuteScoreTransition(this.slides.get(index1 - 1));
+        }
+        if(index2 != this.slides.size()-1) {
+            firstpass += this.slides.get(index1).calcuteScoreTransition(this.slides.get(index2 + 1));
+        }
+        if(index1 != this.slides.size()-1) {
+            firstpass += this.slides.get(index2).calcuteScoreTransition(this.slides.get(index1 + 1));
+        }
+
+
+
+        if(firstpass > 0){
+
+            this.score+= firstpass;
+            Slide temp = this.slides.get(index1);
+            this.slides.set(index1, this.slides.get(index2));
+            this.slides.set(index2, temp);
+        }
+
+        return this.score;
+    }
+
+
+    public void outputSolution() throws IOException {
+        File file = new File("C:\\Users\\guyle\\Desktop\\GoogleHashCode2019\\dataset\\c_memorable_moments_output.txt");
+        file.delete();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(Integer.toString(this.slides.size()) + " ");
+        writer.newLine();
+        for(Slide s : this.slides){
             String res = s.getP0().getId() + " ";
             if(!s.isSingle()){
                 res += s.getP1().getId() + " ";
             }
-            System.out.println(res);
+            writer.write(res);
+            writer.newLine();
         }
+        writer.close();
     }
 
     @Override
