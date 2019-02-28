a56906bc59cea5bf2f5ecd571691119f6c5c1dd1
Remi <rku16@ic.ac.uk>
2019-02-28 17:19:08 +0000

    setup python main

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import KFold
+
+
+
+if __name__ == "__main__":
+    main()
